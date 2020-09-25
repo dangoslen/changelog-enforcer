49dd3f696c05f64d30cba236eeef1e434ef4b112
Dan Goslen <dwgoslen@gmail.com>
2020-09-25 16:55:51 -0400

    Fixes Issue-25 (#27)
    * Initial understanding of the problem; tests coming soon
    
    * Tests complete
    
    * Auto-building distribution
    
    * Adding coverage reporting and badge
    
    * Adjusting regex to support spaces at the begginning and dashes
    
    * Auto-building distribution
    
    * Update fetch command
    
    * Auto-building distribution
    
    * Manually packaging for distribution
    
    * Auto-building distribution
    
    * Attempting to use proper fetch command
    
    * Debugging
    
    * Moving args around
    
    * Repackage and adds changelog

--- src/changelog-enforcer.js
+++ src/changelog-enforcer.js
@@ -14,32 +14,57 @@ module.exports.enforce = async function() {
         const baseRef = pullRequest.base.ref
 
         if (!labelNames.includes(skipLabel)) {
-            let output = ''
-            const options = {}
-            options.listeners = {
-                stdout: (data) => {
-                    output += data.toString();
-                }
-            }
-        
-            await exec.exec('git', ['diff', `origin/${baseRef}`, '--name-status', '--diff-filter=AM'], options)
-
-            const fileNames = generateUpdatedFileList(output)   
-            if (!fileNames.includes(changeLogPath)) {
-                throw new Error(`No update to ${changeLogPath} found!`)
-            }
+            await ensureBranchExists(baseRef)
+            await checkChangeLog(baseRef, changeLogPath)
         }
     } catch(error) {
         core.setFailed(error.message);
     }
 };
 
-function generateUpdatedFileList(output) {
+async function ensureBranchExists(baseRef) {
+    let output = ''
+    const options = {}
+    options.listeners = {
+        stdout: (data) => {
+            output += data.toString();
+        }
+    }
+
+    await exec.exec('git', ['branch', '--verbose', '--all'], options)
+
+    const branches = output.split(/\r?\n/)
+    let branchNames = []
+    branches.map(change => {
+        const branchName = change.replace(/(^\s*[\.\w+/-]*)(\s*)([\w+].*)\n?$/g, '$1').trim()
+        branchNames.push(branchName)
+    })
+
+    if (!branchNames.includes(`remotes/origin/${baseRef}`)) {
+        await exec.exec('git', ['-c', 'protocol.version=2', 'fetch', '--depth=1', 'origin', `${baseRef}`], {})
+    }
+}
+
+async function checkChangeLog(baseRef, changeLogPath) {
+    let output = ''
+    const options = {}
+    options.listeners = {
+        stdout: (data) => {
+            output += data.toString();
+        }
+    }
+    
+    await exec.exec('git', ['diff', `origin/${baseRef}`, '--name-status', '--diff-filter=AM'], options)
+
     const changes = output.split(/\r?\n/)
     let fileNames = []
     changes.map(change => {
         const fileName = change.replace(/(^[A-Z])(\s*)(.*)(\n)?$/g, '$3')
         fileNames.push(fileName)
     })
-    return fileNames;
+    
+    if (!fileNames.includes(changeLogPath)) {
+        throw new Error(`No update to ${changeLogPath} found!`)
+    }
 }
+
