f946936056b9e371eb6d18ec4cce80abce490a57
Dan Goslen <dgoslen@bandwidth.com>
2021-03-01 17:09:00 -0500

    Fixing some names

--- __tests__/label-extractor.test.js
+++ __tests__/label-extractor.test.js
@@ -1,4 +1,4 @@
-const versionExtractor = require('../src/label-extractor')
+const labelExtractor = require('../src/label-extractor')
 
 const EXPECTED_LABELS = ['label-1','label-2_with_underscore','special-[characters](please)']
 const EXPECTED_LABELS_SPACES = ['label 1','label 2_with_underscore','special [characters] (please)']
@@ -7,25 +7,25 @@ const EXPECTED_SINGLE_ENTRY = ['no changelog entry needed']
 describe('the verstion-extractor', () => {
 
   it('should return all labels', () => {
-    const labels = versionExtractor.getLabels('label-1,label-2_with_underscore,special-[characters](please)')
+    const labels = labelExtractor.extractLabels('label-1,label-2_with_underscore,special-[characters](please)')
 
     expect(labels).toStrictEqual(EXPECTED_LABELS)
   })
   
   it('should return all labels when spaces are included and trailing comma', () => {
-    const labels = versionExtractor.getLabels('label-1 ,    label-2_with_underscore   ,      special-[characters](please),')
+    const labels = labelExtractor.extractLabels('label-1 ,    label-2_with_underscore   ,      special-[characters](please),')
 
     expect(labels).toStrictEqual(EXPECTED_LABELS)
   })
 
   it('should return all labels with spaces', () => {
-    const labels = versionExtractor.getLabels('label 1,label 2_with_underscore,special [characters] (please)')
+    const labels = labelExtractor.extractLabels('label 1,label 2_with_underscore,special [characters] (please)')
 
     expect(labels).toStrictEqual(EXPECTED_LABELS_SPACES)
   })
 
   it('should return only a single labels with spaces', () => {
-    const labels = versionExtractor.getLabels('no changelog entry needed')
+    const labels = labelExtractor.extractLabels('no changelog entry needed')
 
     expect(labels).toStrictEqual(EXPECTED_SINGLE_ENTRY)
   })
