be395ca4c3d09f5bf4a0e84d68d1ce96aeef1db7
dangoslen <dangoslen@users.noreply.github.com>
2021-10-31 13:33:52 +0000

    Auto-package action

--- dist/index.js
+++ dist/index.js
@@ -6114,7 +6114,7 @@ function shouldEnforceVersion(expectedLatestVersion) {
     return expectedLatestVersion === ''
 }
 
-async function checkChangeLog(octokit, repository, pullRequestNumber,  changeLogPath, missingUpdateErrorMessage) {
+async function checkChangeLog(octokit, repository, pullRequestNumber, changeLogPath, missingUpdateErrorMessage) {
     core.debug(`Downloading pull request files from  /repos/${repository}/pulls/${pullRequestNumber}/files`)
     const response = await octokit.paginate("GET /repos/{repo}/pulls/{number}/files", {
         repo: repository,
