e58e94b3aaa6dd61a62d2263ec55a5875ecaa9a6
Marcus Noble <AverageMarcus@users.noreply.github.com>
2023-07-20 19:10:43 +0100

    Support skipLabels that contain forward slash (#254)
    * Support skipLabels that contain forward slash

--- __tests__/label-extractor.test.js
+++ __tests__/label-extractor.test.js
@@ -4,14 +4,14 @@ const EXPECTED_LABELS = ['label-1','label-2_with_underscore','special-[character
 const EXPECTED_LABELS_SPACES = ['label 1','label 2_with_underscore','special [characters] (please)']
 const EXPECTED_SINGLE_ENTRY = ['no changelog entry needed']
 
-describe('the verstion-extractor', () => {
+describe('the label-extractor', () => {
 
   it('should return all labels', () => {
     const labels = labelExtractor.extractLabels('label-1,label-2_with_underscore,special-[characters](please)')
 
     expect(labels).toStrictEqual(EXPECTED_LABELS)
   })
-  
+
   it('should return all labels when spaces are included and trailing comma', () => {
     const labels = labelExtractor.extractLabels('label-1 ,    label-2_with_underscore   ,      special-[characters](please),')
 
@@ -28,5 +28,17 @@ describe('the verstion-extractor', () => {
     const labels = labelExtractor.extractLabels('no changelog entry needed')
 
     expect(labels).toStrictEqual(EXPECTED_SINGLE_ENTRY)
+  })
+
+  it('should handle labels containing a forward slash', () => {
+    const labels = labelExtractor.extractLabels('skip/changelog')
+
+    expect(labels).toStrictEqual(['skip/changelog'])
+  })
+
+  it('should handle multiple labels containing a forward slash', () => {
+    const labels = labelExtractor.extractLabels('skip/changelog,no/changelog')
+
+    expect(labels).toStrictEqual(['skip/changelog', 'no/changelog'])
   })
 })
