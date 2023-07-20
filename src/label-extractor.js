eb24d3223f94235046a6d7657614b288670f9fe0
Marcus Noble <github@marcusnoble.co.uk>
2023-07-20 15:47:34 +0100

    Updated comment
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

--- src/label-extractor.js
+++ src/label-extractor.js
@@ -1,6 +1,6 @@
 module.exports.extractLabels = function (labelsString) {
     // Parses a list of labels. Each label can be of any length and will either end with a comma or be the end of the string.
-    // Matches words (\w), whitespace characters (\s), dashes (-), plus signs (+), questions marks (\?), semi-colons (;), brackets (\[\]) and parenthesis (\(\))
+    // Matches words (\w), whitespace characters (\s), dashes (-), plus signs (+), questions marks (\?), semi-colons (;), brackets (\[\]), parenthesis (\(\)) and forward-slashes (\/)
     // Each match may are may not have a trailing comma (,?). If one exists, it is removed before appending it to the list
     const regex = new RegExp(/([\w\s-\/+\?;\[\]\(\)]+,?)/, 'g')
     let labels = []
